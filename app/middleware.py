9df4b1fcbcdfc37733bb4115d8b8b6ab0d92301b
abanishkhatry <khatriavanish@gmail.com>
2025-06-27 17:43:31 -0500

    Changed to accepting anytype of rate limiters

--- app/middleware.py
+++ app/middleware.py
@@ -3,19 +3,16 @@
 # wraps: a Python tool to preserve function metadata when wrapping a function
 from flask import request, jsonify
 from functools import wraps
-from .limiter import FixedWindowRateLimiter
 
 """
 Creating a system that can sit in front of API route and check if the user is 
 under limit request limit. 
 """
-def rate_limit(redis_client): 
-    limiter = FixedWindowRateLimiter(redis_client)
-
+def rate_limit(limiter): 
     def decorator(f): 
         @wraps(f)
         def wrapper(*args, **kwargs):
-          user_ip = request.remote_addr
+          user_ip = request.remote_addr 
           if limiter.is_allowed(user_ip): 
              return f(*args, **kwargs)
           else: 
