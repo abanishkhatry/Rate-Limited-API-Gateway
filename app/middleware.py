25c6fbbf7c3ee10c8855d9cbcdb3b8f1d2a85c20
abanishkhatry <khatriavanish@gmail.com>
2025-07-03 15:01:37 -0500

    Changing user's identity accessing to id from ip address.

--- app/middleware.py
+++ app/middleware.py
@@ -4,6 +4,7 @@
 from flask import request, jsonify
 from functools import wraps
 from limiter_factory import get_limiter_for_route
+from auth import decode_token 
 
 """
 Creating a system that can sit in front of API route and check if the user is 
@@ -14,11 +15,16 @@ def config_aware_rate_limiter(redis_client):
         @wraps(f)
         def wrapper(*args, **kwargs):
           route = request.path
-          user_ip = request.remote_addr 
+          token_payload = decode_token()
 
+          if token_payload is None : 
+             return jsonify({"error": "Unauthorized – valid token required"}), 401
+          
+          user_id = token_payload.get("user_id")
+          role = token_payload.get("role")
           try: 
-             limiter = get_limiter_for_route(route, redis_client)
-             if not limiter.is_allowed(user_ip): 
+             limiter = get_limiter_for_route(route, redis_client,role)
+             if not limiter.is_allowed(user_id): 
                 return jsonify({"error": "Too Many Requests"}), 429
              
           except ValueError as e: 
