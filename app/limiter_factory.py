cd6f7644eecbedfe18ad9a8429e1d90f5dd70d6d
abanishkhatry <khatriavanish@gmail.com>
2025-07-03 14:59:20 -0500

    Including role parameter to decide the algo and limits accordingly.

--- app/limiter_factory.py
+++ app/limiter_factory.py
@@ -2,24 +2,30 @@ import os
 from limiters.fixed_window import FixedWindowRateLimiter
 from limiters.sliding_window import SlidingWindowRateLimiter
 from limiters.token_bucket import TokenBucketRateLimiter
-from rate_limit_config import rate_limit_config
+from rate_limit_config import  RATE_LIMIT_CONFIG
 
 
-def get_limiter_for_route(route, redis_client) : 
-    config = rate_limit_config.get(route)
-    if not config: 
+def get_limiter_for_route(route, redis_client, role) : 
+    
+    if route not in RATE_LIMIT_CONFIG: 
         raise ValueError(f"No rate limiting config found for route: {route}")
     
-    algo = config["algo"]
-    params = config["params"]
+    role_config = RATE_LIMIT_CONFIG[route].get(role)
 
-    if algo == "sliding_window": 
-        return SlidingWindowRateLimiter(redis_client, **params)
+    if not role_config: 
+        raise ValueError(f"No config for role '{role} on route {route}")
     
-    elif algo == "token_bucket" : 
-        return TokenBucketRateLimiter(redis_client,**params)
-    elif algo == "fixed_window": 
-        return FixedWindowRateLimiter(redis_client, **params)
+    algo_type = role_config["type"]
+    limit = role_config["requests"]
+    window = role_config["window"]
+
+    if algo_type == "sliding": 
+        return SlidingWindowRateLimiter(redis_client, limit, window)
+    
+    elif algo_type == "token" : 
+        return TokenBucketRateLimiter(redis_client,limit, window)
+    elif algo_type == "fixed": 
+        return FixedWindowRateLimiter(redis_client, limit, window)
     
     else:
-        raise ValueError(f"Unsupported algorithm type: {algo}")
+        raise ValueError(f"Unsupported algorithm type: {algo_type}")
