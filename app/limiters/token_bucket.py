db80e279f5e372be01c6135dadb9a191e50a0ba7
abanishkhatry <khatriavanish@gmail.com>
2025-08-02 22:17:44 -0400

    Updated is_allowed in Token Bucket algorithm

--- app/limiters/token_bucket.py
+++ app/limiters/token_bucket.py
@@ -22,19 +22,14 @@ class TokenBucketRateLimiter(BaseRateLimiter):
     
     # checking if the user can do something right now based on how many tokens they have. 
     def is_allowed(self, user_id: str) -> bool:
-        # stores how many tokens they have left. 
         token_key = f"token_bucket:tokens:{user_id}"
-        # stores when we last added tokens
         time_key = f"token_bucket:timestamp:{user_id}"
-        
+
         current_time = time.time()
 
-        # Step 1: Get last known values from Redis
         token_count = self.redis.get(token_key)
         last_refill_time = self.redis.get(time_key)
 
-        # Step 2: Parse values or initialize
-        # for first time user, filling their values. 
         if token_count is None or last_refill_time is None:
             token_count = self.bucket_capacity
             last_refill_time = current_time
@@ -42,21 +37,15 @@ class TokenBucketRateLimiter(BaseRateLimiter):
             token_count = float(token_count)
             last_refill_time = float(last_refill_time)
 
-        # Step 3: Calculate tokens to refill
-        # checking how long it has been since they have refilled
         time_passed = current_time - last_refill_time
-        # getting number of tokens to refill.
         refill_tokens = time_passed * self.refill_rate
-        # updating the token count
         token_count = min(self.bucket_capacity, token_count + refill_tokens)
 
-        # if they have atleast one token, letting them through, else not and updating their time. 
-        if token_count >= 1:
+        allowed = token_count >= 1
+        if allowed:
             token_count -= 1
-            self.redis.set(token_key, token_count)
             self.redis.set(time_key, current_time)
-            return True
-        else:
-            self.redis.set(token_key, token_count)
-            self.redis.set(time_key, current_time)
-            return False
+
+        self.redis.set(token_key, token_count)
+        return allowed
+
